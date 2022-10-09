4399a8544d960bcdf4c7d44005f9e4ca1b1fb79b
maikls <maikls.morozovs@va.lv>
2022-10-09 12:06:25 +0000

    Pievienots API key

--- naked.py
+++ naked.py
@@ -11,7 +11,7 @@ print('Asteroid processing service')
 print('Loading configuration from file')
 
 # 
-nasa_api_key = "???"
+nasa_api_key = "S02Nzx0IK6qzNnMB4kQxrTzrXt2lMdUBedZ1fhr4"
 nasa_api_url = "https://api.nasa.gov/neo/"
 
 # Getting todays date
